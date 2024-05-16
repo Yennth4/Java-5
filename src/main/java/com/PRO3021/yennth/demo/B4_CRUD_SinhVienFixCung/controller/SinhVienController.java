fb92c82978255ba77ed5bd1b07adbe4ff93d7859
Hai Yen Nguyen <haiyenng411@gmail.com>
2024-05-16 18:27:35 +0700

    B4

--- src/main/java/com/PRO3021/yennth/demo/B4_CRUD_SinhVienFixCung/controller/SinhVienController.java
+++ src/main/java/com/PRO3021/yennth/demo/B4_CRUD_SinhVienFixCung/controller/SinhVienController.java
@@ -1,26 +1,34 @@
 package com.PRO3021.yennth.demo.B4_CRUD_SinhVienFixCung.controller;
+
 import com.PRO3021.yennth.demo.B4_CRUD_SinhVienFixCung.service.SinhVienService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/sinh-vien/") // tach duong link chung
 public class SinhVienController {
 
     private SinhVienService service = new SinhVienService();
 
-    @GetMapping("/sinh-vien/hien-thi")
+    @GetMapping("hien-thi")
     public String hienThi(Model model){
         model.addAttribute( "listSV", service.getAll());
         return "/demo/B4/sinhviens";
     }
 
-    @GetMapping("/sinh-vien/remove/{ma}")
+    @GetMapping("remove/{ma}")
     public String delete(@PathVariable String ma) {
         service.xoaSinhVien(ma);
         return "redirect:/sinh-vien/hien-thi";
     }
 
+    @RequestMapping(value = "add" , method = RequestMethod.GET)
+    // cach 2
+    public String add() {
+        return "";
+    }
 }
